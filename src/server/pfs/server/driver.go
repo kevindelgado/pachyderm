d5738351d116005bc9fc1be07d1eb9a3e3c253d8
Bryce McAnally <bryce@pachyderm.io>
2019-02-18 11:17:25 -0800

    Refactor reading of hashtree shards into merge reader abstraction

--- src/server/pfs/server/driver.go
+++ src/server/pfs/server/driver.go
@@ -2521,7 +2521,11 @@ func (d *driver) getFile(pachClient *client.APIClient, file *pfs.File, offset in
 	blockRefs := []*pfs.BlockRef{}
 	var totalSize int64
 	var found bool
-	if err := hashtree.Glob(rs, file.Path, func(path string, node *hashtree.NodeProto) error {
+	mr, err := hashtree.NewMergeReader(rs)
+	if err != nil {
+		return nil, err
+	}
+	if err := mr.Glob(file.Path, func(path string, node *hashtree.NodeProto) error {
 		if node.FileNode == nil {
 			return nil
 		}
@@ -2738,7 +2742,11 @@ func (d *driver) inspectFile(pachClient *client.APIClient, file *pfs.File) (fi *
 			}
 		}
 	}()
-	node, err := hashtree.Get(rs, file.Path)
+	mr, err := hashtree.NewMergeReader(rs)
+	if err != nil {
+		return nil, err
+	}
+	node, err := mr.Get(file.Path)
 	if err != nil {
 		return nil, pfsserver.ErrFileNotFound{file}
 	}
@@ -2810,7 +2818,11 @@ func (d *driver) listFile(pachClient *client.APIClient, file *pfs.File, full boo
 			}
 		}
 	}()
-	return hashtree.List(rs, file.Path, func(path string, node *hashtree.NodeProto) error {
+	mr, err := hashtree.NewMergeReader(rs)
+	if err != nil {
+		return err
+	}
+	return mr.List(file.Path, func(path string, node *hashtree.NodeProto) error {
 		if history != 0 {
 			return d.fileHistory(pachClient, client.NewFile(file.Commit.Repo.Name, file.Commit.ID, path), history, f)
 		}
@@ -2893,7 +2905,11 @@ func (d *driver) walkFile(pachClient *client.APIClient, file *pfs.File, f func(*
 			}
 		}
 	}()
-	return hashtree.Walk(rs, file.Path, func(path string, node *hashtree.NodeProto) error {
+	mr, err := hashtree.NewMergeReader(rs)
+	if err != nil {
+		return err
+	}
+	return mr.Walk(file.Path, func(path string, node *hashtree.NodeProto) error {
 		return f(nodeToFileInfo(commitInfo, path, node, false))
 	})
 }
@@ -2949,7 +2965,11 @@ func (d *driver) globFile(pachClient *client.APIClient, commit *pfs.Commit, patt
 			}
 		}
 	}()
-	return hashtree.Glob(rs, pattern, func(rootPath string, rootNode *hashtree.NodeProto) error {
+	mr, err := hashtree.NewMergeReader(rs)
+	if err != nil {
+		return err
+	}
+	return mr.Glob(pattern, func(rootPath string, rootNode *hashtree.NodeProto) error {
 		return f(nodeToFileInfo(commitInfo, rootPath, rootNode, false))
 	})
 }
